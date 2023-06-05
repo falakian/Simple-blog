de535a3a60018560519ab9188394393f920b6aa2
falakian <ali.falakian20@gamil.com>
2023-06-05 17:42:23 +0330

    Posts, editing and deleting posts and errors 404 and 403

--- blog/routes.py
+++ blog/routes.py
@@ -1,19 +1,63 @@
-from flask import render_template , redirect , url_for , flash , request
+from flask import render_template , redirect , url_for , flash , request , abort
 from blog import app , db , bcrypt
-from blog.form import RegistrationForm , LoginForm , UpdateProfileForm
+from blog.form import RegistrationForm , LoginForm , UpdateProfileForm , NewPostForm
 from blog.models import User , Post
 from flask_login import login_user , current_user , logout_user , login_required
 
 
-@app.route("/" , methods=['GET'])
-def home():
-    return render_template('home.html')
+@app.route("/index/<int:page>" , methods=['GET'])
+def home(page=1):
+    per_page=3
+    posts = Post.query.paginate(page=page ,per_page=per_page ,error_out= False).items
+    return render_template('home.html' , posts=posts)
+
+@app.route("/post/<int:post_id>")
+def detail(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('detail.html', post=post)
+
+@app.route("/post/<int:post_id>/delete")
+def delete(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user :
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been successfully deleted' , 'info')
+    return redirect(url_for('home' , page=1))
+
+
+@app.route("/post/<int:post_id>/update" , methods=['GET','POST'])
+def update(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user :
+        abort(403)
+    form = NewPostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been successfully updated' , 'info')
+        return redirect(url_for('detail' , post_id=post.id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('update.html' , form=form)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 @app.route("/register", methods=['GET','POST'])
 def register():
     if current_user.is_authenticated:
         flash('You are already logged in' , 'success' )
-        return redirect(url_for('home'))
+        return redirect(url_for('home' , page=1))
     form = RegistrationForm()
     if form.validate_on_submit():
         hased_pass = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -21,13 +65,13 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('You have successfully registered','success')
-        return redirect(url_for('home'))
+        return redirect(url_for('home' , page=1))
     return render_template('register.html' , form=form)
 @app.route("/login" , methods=['GET','POST'])
 def login():
     if current_user.is_authenticated:
         flash('You are already logged in' , 'success' )
-        return redirect(url_for('home'))
+        return redirect(url_for('home' , page=1))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -45,7 +89,7 @@ def login():
 def logout():
     logout_user()
     flash('You have successfully logged out' , 'success')
-    return redirect(url_for('home'))
+    return redirect(url_for('home' , page=1))
 
 @app.route('/profile')
 @login_required
@@ -66,3 +110,24 @@ def edit_profile():
         form.username.data=current_user.username
         form.email.data=current_user.email
     return render_template('edit_profile.html' , form=form)
+
+@app.route('/new_post' , methods=['GET','POST'])
+@login_required
+def new_post():
+    form=NewPostForm()
+    if form.validate_on_submit():
+        post=Post(title=form.title.data , content=form.content.data , author =current_user )
+        db.session.add(post)
+        db.session.commit()
+        flash('The post was created successfully' , 'success')
+        return redirect(url_for('home' , page=1))
+    return render_template('new_post.html' , form=form)
+    
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('403.html'), 403
