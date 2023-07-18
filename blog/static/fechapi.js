866937658bcfd7f429ebb881fbdfb3c5f51be841
falakian <ali.falakian20@gamil.com>
2023-07-18 14:07:15 +0330

    Add requirements.txt

--- blog/static/fechapi.js
+++
@@ -1,7 +0,0 @@
-const url = "http://127.0.0.1:5000/api/fetch";
-
-async function getcount(url) {
-    let x = await fetch(url);
-    let y = await x.text();
-    document.getElementById("count2").innerHTML = `Number of users :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ${y} `;  }
-getcount(url);
