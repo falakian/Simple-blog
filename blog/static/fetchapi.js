8519ba76e1e2159d2efb1da49add0de353259b7f
falakian <ali.falakian20@gamil.com>
2023-07-18 14:03:36 +0330

    Changes to the fetch api file

---
+++ blog/static/fetchapi.js
@@ -0,0 +1,8 @@
+
+   const url = (process.env.BASE_URL + '/api/fetch' || 'http://127.0.0.1:5000/api/fetch');
+
+async function getcount(url) {
+    let x = await fetch(url);
+    let y = await x.text();
+    document.getElementById("count2").innerHTML = `Number of users :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ${y} `;  }
+getcount(url);
